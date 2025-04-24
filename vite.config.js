e5c36e13b0dbd9fcc6ac5f50ea1cc796cef17742
João Pedro Barros <joaoppob@gmail.com>
2025-04-24 17:11:34 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
     base: '/petflix/', // ← nome do repositório no GitHub
     server: {
         port: 3000,
         open: true, // ← Isso aqui abre o navegador automaticamente
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
 });
